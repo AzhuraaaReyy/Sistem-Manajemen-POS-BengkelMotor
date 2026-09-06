61fbb29f279db3994962bf420d78a7c8584c193c
Muhammad Rizal <rizalkurniawanramadhan52@gmail.com>
2026-09-06 20:30:40 +0700

    done

--- scripts/db_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;port=3306', 'root', '', [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]);
-    echo 'CONNECTED: ' . $pdo->query('SELECT VERSION()')->fetchColumn() . PHP_EOL;
-} catch (Exception $e) {
-    echo 'ERR: ' . $e->getMessage() . PHP_EOL;
-}
